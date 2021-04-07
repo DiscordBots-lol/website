27d7e285a589217dc1897a301c9e5d7d04f46653
apollo <mxrcdev@gmail.com>
2021-04-07 19:02:14 +0200

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const session = require("express-session");
+const path = require("path");
 const config = require("../config.json");
 
 const tokenManager = require("./utils/TokenManager");
 const auth = require("./utils/auth");
 const api = require("./utils/api");
+const main = require('./routes/index');
 
 const app = express();
 
-app.set("views", path.join(__dirname + "/../../views"));
-app.use(express.static(path.join(__dirname + "/../../assets/public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/../views"));
+app.use(express.static(path.join(__dirname + "/../../assets/public")));
 
+app.use('/', main);
 
 app.use("/auth", auth);
 
